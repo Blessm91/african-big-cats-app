f95d1b923950a78ca9c7ba74233cb9b3d9ea5535
Blessm91 <aservin.cov@gmail.com>
2025-10-21 23:02:35 -0700

    Add PantheraApp class to implement main application logic for managing big cats

---
+++ src/main/java/com/africanbigcats/PantheraApp.java
@@ -0,0 +1,36 @@
+package com.africanbigcats;
+
+import java.util.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class PantheraApp {
+    public static void main(String[] args) {
+
+        Menu appMenu = new Menu();
+
+        Character command = '_';
+        LinkedList<Panthera> catList = new LinkedList<>();
+
+        // loop until user quits
+        while (command != 'q') {
+
+            // print the menu
+            appMenu.print();
+
+            // get a command
+            System.out.print("Enter a command: ");
+            command = appMenu.getCommand();
+
+            // execute a commmand
+            appMenu.executeCommand(command, catList);
+
+            // move the cats to new positions in africa
+            appMenu.update(catList);
+
+        }
+
+    }
+}
